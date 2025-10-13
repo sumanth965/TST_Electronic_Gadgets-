abb64279e53db32bf43db3d5a90863b20fb5e1dc
sumanth965 <sumanthpoojary965@gmail.com>
2025-10-13 20:34:47 +0530

    updated index html in rooot

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: command === 'build' ? './' : '/', // relative paths only for production
+  base: './', // Important for Render to serve correctly
 }))
