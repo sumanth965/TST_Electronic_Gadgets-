26648e5d9bbd7c2cf16c820935803bb43f842d3b
sumanth965 <sumanthpoojary965@gmail.com>
2025-10-14 20:33:06 +0530

    aaaaaaa

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ command }) => ({
+export default defineConfig({
   plugins: [react()],
-  base: './', // Important for Render to serve correctly
-}))
+  base: './', // ⚠️ this line is very important for Render
+  build: {
+    outDir: 'dist',
+  },
+})
