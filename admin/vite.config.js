e892f8aac803b8c6584884ae6b1674730347bad2
sumanth965 <sumanthpoojary965@gmail.com>
2025-10-16 20:17:42 +0530

    Updated vite config

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // ⚠️ this line is very important for Render
-  build: {
-    outDir: 'dist',
-  },
+  base: './', // ✅ ensures all assets load relative to the root
 })
